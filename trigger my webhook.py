8b7df32945f6d8f26a9781da943d6fbc29e85c8b
hamsananth <78092120+webpicker606@users.noreply.github.com>
2021-06-28 18:50:14 +0530

    update
    just changed the direct string to variable

--- trigger my webhook.py
+++ trigger my webhook.py
@@ -1,4 +1,5 @@
 import webbrowser
 
-webbrowser.open('https://maker.ifttt.com/trigger/motion/with/key/fCTQVuVAUfibXLjDnMw5FZWVLlKtd2gNIQTV2zBTBFR')
+motion_mail_trigger='https://maker.ifttt.com/trigger/motion/with/key/fCTQVuVAUfibXLjDnMw5FZWVLlKtd2gNIQTV2zBTBFR'
+webbrowser.open(motion_mail_trigger)
 print('successfully triggered')
